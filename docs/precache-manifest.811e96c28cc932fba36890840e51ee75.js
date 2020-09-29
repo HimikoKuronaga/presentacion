0ca59e14208596441c9a500c819372b97fe48034
HimikoKuronaga <hugolanguila@gmail.com>
2020-09-29 12:28:55 -0500

    paginas listo

---
+++ docs/precache-manifest.811e96c28cc932fba36890840e51ee75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e4520cc0e0ecbc0069b9a632fa5b03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9657f945dc0fb936ef44",
+    "url": "/static/css/main.b5009cf7.chunk.css"
+  },
+  {
+    "revision": "f0342786c44eb305c8ae",
+    "url": "/static/js/2.2a0fb61e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2a0fb61e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9657f945dc0fb936ef44",
+    "url": "/static/js/main.8ce8e657.chunk.js"
+  },
+  {
+    "revision": "ddb23d41b025de166703",
+    "url": "/static/js/runtime-main.60ded422.js"
+  }
+]);
